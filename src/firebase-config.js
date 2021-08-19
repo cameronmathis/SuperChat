fa766397afbe66c63883022f96d6a411b15668c6
Cameron Mathis <cmmathis98@gmail.com>
2021-08-18 21:51:42 -0400

    adding file needed for deployment

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyC-8HuTyOfO8FLAHw5DylheVYot9pZynOI",
+  authDomain: "superchat-clm.firebaseapp.com",
+  projectId: "superchat-clm",
+  storageBucket: "superchat-clm.appspot.com",
+  messagingSenderId: "726455511635",
+  appId: "1:726455511635:web:d19fd5176f64b1ff23b4a6",
+  measurementId: "G-3PNRCPC3BE",
+};
